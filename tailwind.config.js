d6a220712b9098f2f54c9cf464c82b4b71dae3d4
dothedro <drooooofficial@gmail.com>
2022-12-07 20:03:09 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    fontFamily: {
+      'serif': ['Fraunces'],
+      'sans': ['Montserrat']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
